addc4c94df614fe184dd91ddf7d24186599864c9
Thandeka Sineli <105169241+ThandiSineli@users.noreply.github.com>
2024-03-01 07:10:42 +0200

    Update authMiddleware.js

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,14 @@
+import jwt from 'jsonwebtoken';
+
+const authenticate = (req, res, next) => {
+  let { cookie } = req.headers;
+  let tokenInHeader = cookie.split('=')[1];
+  if (tokenInHeader === null) res.sendStatus(401);
+  jwt.verify(tokenInHeader, process.env.SECRET_key, (err, user) => {
+    if (err || !user) return res.sendStatus(403);
+    req.user = user;
+    next(); // Continue to the next middleware/route
+  });
+};
+
+export default authenticate;
