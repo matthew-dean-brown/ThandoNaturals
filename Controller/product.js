9ee9126895727eacc0c98c6dce49df6b79b54658
Thandeka Sineli <105169241+ThandiSineli@users.noreply.github.com>
2024-03-01 14:18:52 +0200

    Update product.js

--- Controller/product.js
+++ Controller/product.js
@@ -1,28 +1,34 @@
-import { getproducts, getproduct, addproduct, deleteproduct, updateproduct } from '../models/database';
+import {
+  getproducts,
+  getproduct,
+  addproduct,
+  deleteproduct,
+  updateproduct,
+} from '../models/database.js';
 
 // Get all products
-export const getProducts = async (req, res) => {
+ const getProducts = async (req, res) => {
   res.send(await getproducts());
 };
 
 // Get product by ID
-export const getProduct = async (req, res) => {
+const getProduct = async (req, res) => {
   res.send(await getproduct(+req.params.prodID));
 };
 
 // Add a product
-export const addProduct = async (req, res) => {
+const addProduct = async (req, res) => {
   const { prodID, prodName, quantity, amount, category, prodUrl } = req.body;
   res.send(await addproduct(prodID, prodName, quantity, amount, category, prodUrl));
 };
 
 // Delete a product
-export const deleteProduct = async (req, res) => {
+ const deleteProduct = async (req, res) => {
   res.send(await deleteproduct(req.params.prodID));
 };
 
 // Update a product
-export const updateProduct = async (req, res) => {
+let updateProduct = async (req, res) => {
   try {
     const { prodName, prodUrl, quantity, amount, category } = req.body;
     await updateproduct(prodName, prodUrl, quantity, amount, category, +req.params.prodID);
@@ -32,3 +38,4 @@ export const updateProduct = async (req, res) => {
     res.status(500).json({ msg: 'Internal Server Error' });
   }
 };
+export{getProducts,getProduct,addProduct,deleteProduct,updateProduct,}
